c219f95425c35eb5ad01b9d5c5d9dd8135e8ee06
Roberto Milanese <r.milanese1@studenti.unimol.it>
2022-12-09 17:44:10 +0100

    Implement native startActivity method

--- android/src/main/java/it/unimol/intentsender/IntentSenderPlugin.java
+++ android/src/main/java/it/unimol/intentsender/IntentSenderPlugin.java
@@ -1,22 +1,75 @@
 package it.unimol.intentsender;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.Iterator;
+
 @CapacitorPlugin(name = "IntentSender")
 public class IntentSenderPlugin extends Plugin {
 
-    private IntentSender implementation = new IntentSender();
-
     @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    public void startActivity(PluginCall call) {
+        try {
+            this.getContext().startActivity(
+                    IntentSenderPlugin.parseIntent(call.getData()));
+        } catch (ActivityNotFoundException e) {
+            call.reject("No activity found for the given intent");
+        }
+    }
 
-        JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
-        call.resolve(ret);
+    private static Intent parseIntent(JSObject data) {
+        Intent intent = new Intent(data.getString("action"));
+
+        String intentData = data.getString("data");
+
+        if (intentData != null)
+            intent.setData(Uri.parse(intentData));
+
+        try {
+            JSONArray categories = data.getJSONArray("categories");
+
+            for (int i = 0; i < categories.length(); i++)
+                intent.addCategory(categories.getString(i));
+        } catch (JSONException ignored) {
+        }
+
+        String type = data.getString("type");
+
+        if (type != null)
+            intent.setTypeAndNormalize(type);
+
+        JSObject component = data.getJSObject("component");
+
+        if (component != null)
+            intent.setClassName(
+                    component.getString("package"),
+                    component.getString("class"));
+
+        JSObject extras = data.getJSObject("extras");
+
+        if (extras != null) {
+            Iterator<String> keys = extras.keys();
+
+            while (keys.hasNext()) {
+                String key = keys.next();
+
+                intent.putExtra(key, extras.getString(key));
+            }
+        }
+
+        intent.setFlags(data.getInteger("flags", 0));
+
+        return intent;
     }
 }
