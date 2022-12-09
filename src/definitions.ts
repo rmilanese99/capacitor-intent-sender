6cd6d77054eefd7b906461ca3a18723a74cd4b90
Roberto Milanese <r.milanese1@studenti.unimol.it>
2022-12-09 16:16:11 +0100

    Add Intent interface to definitions.ts

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,43 @@
 export interface IntentSenderPlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
+}
+
+export interface Intent {
+
+    /**
+     * The action to be performed
+     */
+    action: string;
+
+    /**
+     * The data to operate on, expressed as a URI
+     */
+    data?: string;
+
+    /**
+     * Additional information about the given action
+     */
+    categories?: string[];
+
+    /**
+     * The explicit type of the given data
+     */
+    type?: string;
+
+    /**
+     * The explicit name of the target component class
+     */
+    component?: {
+        package: string;
+        class: string;
+    };
+
+    /**
+     * Any additional information to be passed to the target component
+     */
+    extras?: { [key: string]: unknown };
+
+    /**
+     * Special flags controlling how the Intent is handled
+     */
+    flags?: number;
 }
